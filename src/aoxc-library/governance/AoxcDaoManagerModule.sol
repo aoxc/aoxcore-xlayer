16e660e7dc0d91e5bb652997b4a9270e0d55f074
AOXC <aoxcdao@gmail.com>
2026-03-16 14:59:41 +0300

    Fix DaoManager module constructor wiring and document usage (#62)

--- src/aoxc-library/governance/AoxcDaoManagerModule.sol
+++ src/aoxc-library/governance/AoxcDaoManagerModule.sol
@@ -4,4 +4,7 @@ pragma solidity 0.8.33;
 import {AoxcDaoManager} from "aoxc-gov/AoxcDaoManager.sol";
 
 /// @notice Library entrypoint for AOXC governance consumers.
-contract AoxcDaoManagerModule is AoxcDaoManager {}
+/// @dev Thin wrapper that forwards constructor args to the maintained V2 governance implementation.
+contract AoxcDaoManagerModule is AoxcDaoManager {
+    constructor(address registry_, address token_, uint256 lifespan_) AoxcDaoManager(registry_, token_, lifespan_) {}
+}
